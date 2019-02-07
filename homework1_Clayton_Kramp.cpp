5a3a9fba3ee4615eef6999ad35c47ee9d09c2258
krampcg23 <krampcg23@gmail.com>
2019-02-07 14:16:10 -0700

    Fixed race conditions

--- homework1_Clayton_Kramp.cpp
+++ homework1_Clayton_Kramp.cpp
@@ -3,21 +3,35 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ctime>
+#include <vector>
 #define BASE 100
 
 using namespace std;
 
-void vadd (int* arr, int& sum, int& largest, int n) {
+unsigned* vadd (int* arr, int n) {
 	if (n <= BASE) {
+		unsigned sum = 0;
+		unsigned largest = 0;
+		unsigned* vec = new unsigned[2];
 		for (int i = 0; i < n; i++) {
 			sum += arr[i];
 			if(arr[i] > largest) largest = arr[i];
 		}
+		vec[0] = sum; vec[1] = largest;
+		return vec;
 	}
 	else {
-		cilk_spawn vadd(arr, sum, largest, n/2);
-		vadd (arr + n/2, sum, largest, n - n/2);
+		unsigned* v1 = new unsigned[2];
+		unsigned* v2 = new unsigned[2];
+		v1 = cilk_spawn vadd(arr, n/2);
+		v2 = cilk_spawn vadd (arr + n/2, n - n/2);
 		cilk_sync;
+		unsigned* returning = new unsigned[2];
+		returning[0] = v1[0] + v2[0];
+		if (v1[1] > v2[1]) returning[1] = v1[1];
+		else returning[1] = v2[1];
+		delete[] v1; delete[] v2;
+		return returning;
 	}
 } 
 
@@ -38,14 +52,13 @@ int main(int argc, char* argv[]) {
 		arr[i] = temp;
 	}
 
-	int sum = 0;
-	int largest = 0;
-
-	vadd(arr, sum, largest, N);
+	unsigned* vec = new unsigned[2];
+	vec = vadd(arr, N);
 	
         delete[] arr;
 //	cout << "Time Elapsed: " << (double)(clock() - tStart) / CLOCKS_PER_SEC << endl;
 
-	cout << "Maximum: " << largest << "; Sum: " << sum << endl;
+	cout << "Maximum: " << vec[1] << "; Sum: " << vec[0] << endl;
+	delete[] vec;
 	return 0;
 }
